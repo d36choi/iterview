9b1ac184f62f1da53d1cfd848fbf6499107fb268
d36choi <yuio1126@gmail.com>
2021-03-14 22:29:52 +0900

    Initial commit

---
+++ src/main/java/com/example/iterview/IterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.iterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iterview/IterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
